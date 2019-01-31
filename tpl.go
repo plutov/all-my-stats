9ce3c82e1d52005468666f8a6da50fdf49b95ffb
Alex Pliutau <a.pliutau@gmail.com>
2019-01-31 16:29:46 +0100

    line fix

--- tpl.go
+++ tpl.go
@@ -6,4 +6,5 @@ YouTube Subscribers: {{.YouTubeSubscribers}}
 YouTube Views: {{.YouTubeViews}}
 pliutau.com users (30 days): {{.GAUsers30Days}}
 pliutau.com top 5 pages (30 days):
-{{ range $key, $val := .GAPages30Days }}{{ $val }} | {{ $key }}{{ end }}`
+{{ range $key, $val := .GAPages30Days }}{{ $val }} | {{ $key }}
+{{ end }}`
